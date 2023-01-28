3064fab642163de12f90a7fcf0859035d1f8999d
WHITEJAIL <tzlUltraGames@outlook.com>
2023-01-28 21:54:16 +0800

    增加数组排序的蓝图函数

--- Source/FireflyFunctionLibraries/Private/FireflyAlgorithmLibrary.cpp
+++ Source/FireflyFunctionLibraries/Private/FireflyAlgorithmLibrary.cpp
@@ -3,30 +3,79 @@
 
 #include "FireflyAlgorithmLibrary.h"
 
-TArray<float> UFireflyAlgorithmLibrary::MakeSortedFloatArray(const UObject* WorldContextObject,
-	TArray<float> InArray, bool bOrderFromSmall)
+struct FFireflySortByDistance
 {
-	TArray<float> OutArray;
-	Algo::Sort(OutArray);
+    explicit FFireflySortByDistance(const FVector& InSourceLocation)
+        : SourceLocation(InSourceLocation)
+    {
+    }
+
+    /* The Location to use in our Sort comparison. */
+    FVector SourceLocation;
+
+    bool operator()(const AActor* A, const AActor* B) const
+    {
+        float DistanceA = FVector::DistSquared(SourceLocation, A->GetActorLocation());
+        float DistanceB = FVector::DistSquared(SourceLocation, B->GetActorLocation());
+
+        return DistanceA < DistanceB;
+    }
+
+    bool operator()(const FHitResult& A, const FHitResult& B) const
+    {
+        float DistanceA = FVector::DistSquared(SourceLocation, A.GetActor()->GetActorLocation());
+        float DistanceB = FVector::DistSquared(SourceLocation, B.GetActor()->GetActorLocation());
+
+        return DistanceA < DistanceB;
+    }
+};
+
+TArray<float> UFireflyAlgorithmLibrary::FloatArray_Sort(TArray<float> InArray, bool bOrderFromSmall)
+{
+	Algo::Sort(InArray);
 
 	if (!bOrderFromSmall)
 	{
-		Algo::Reverse(OutArray);
+		Algo::Reverse(InArray);
 	}
 
-	return OutArray;
+    return InArray;
 }
 
-TArray<int32> UFireflyAlgorithmLibrary::MakeSortedIntArray(const UObject* WorldContextObject,
-	TArray<int32> InArray, bool bOrderFromSmall)
+TArray<int32> UFireflyAlgorithmLibrary::IntArray_Sort(TArray<int32> InArray, bool bOrderFromSmall)
 {
-	TArray<int32> OutArray;
-	Algo::Sort(OutArray);
+	Algo::Sort(InArray);
 
 	if (!bOrderFromSmall)
 	{
-		Algo::Reverse(OutArray);
+		Algo::Reverse(InArray);
 	}
 
-	return OutArray;
+    return InArray;
+}
+
+TArray<AActor*> UFireflyAlgorithmLibrary::ActorArray_SortByDistance(TArray<AActor*> InArray, 
+	FVector InLocation, bool bOrderFromSmall)
+{
+	Algo::Sort(InArray, FFireflySortByDistance(InLocation));
+
+    if (!bOrderFromSmall)
+	{
+		Algo::Reverse(InArray);
+	}
+
+    return InArray;
+}
+
+TArray<FHitResult> UFireflyAlgorithmLibrary::HitResultArray_SortByDistance(TArray<FHitResult> InArray, 
+	FVector InLocation, bool bOrderFromSmall)
+{
+    Algo::Sort(InArray, FFireflySortByDistance(InLocation));
+
+    if (!bOrderFromSmall)
+	{
+		Algo::Reverse(InArray);
+	}
+
+    return InArray;
 }
--- Source/FireflyFunctionLibraries/Public/FireflyAlgorithmLibrary.h
+++ Source/FireflyFunctionLibraries/Public/FireflyAlgorithmLibrary.h
@@ -17,13 +17,21 @@ class FIREFLYFUNCTIONLIBRARIES_API UFireflyAlgorithmLibrary : public UBlueprintF
 #pragma region Array 数组
 
 public:
-	/** 为float数组排序，并返回排序后的数组。bOrderFromSmall为true，则从小到大排序，为false，则从大到小排序 */
-	UFUNCTION(BlueprintCallable, Category = "LegendSystem|Algorith", meta = (WorldContext = "WorldContextObject"))
-	static TArray<float> MakeSortedFloatArray(const UObject* WorldContextObject, TArray<float> InArray, bool bOrderFromSmall = true);
+	/** 为float数组排序，bOrderFromSmall为true，则从小到大排序，为false，则从大到小排序 */
+	UFUNCTION(BlueprintCallable, Category = "LegendSystem|Algorith")
+	static TArray<float> FloatArray_Sort(TArray<float> InArray, bool bOrderFromSmall = true);
 
-	/** 为int32数组排序，并返回排序后的数组。bOrderFromSmall为true，则从小到大排序，为false，则从大到小排序 */
-	UFUNCTION(BlueprintCallable, Category = "LegendSystem|Algorith", meta = (WorldContext = "WorldContextObject"))
-	static TArray<int32> MakeSortedIntArray(const UObject* WorldContextObject, TArray<int32> InArray, bool bOrderFromSmall = true);
+	/** 为int32数组排序，bOrderFromSmall为true，则从小到大排序，为false，则从大到小排序 */
+	UFUNCTION(BlueprintCallable, Category = "LegendSystem|Algorith")
+	static TArray<int32> IntArray_Sort(TArray<int32> InArray, bool bOrderFromSmall = true);
+
+	/** 为Actor引用数组排序，排序以按照Actor与给定位置间的距离排列 */
+	UFUNCTION(BlueprintCallable, Category = "LegendSystem|Algorith")
+	static TArray<AActor*> ActorArray_SortByDistance(TArray<AActor*> InArray, FVector InLocation, bool bOrderFromSmall = true);
+
+	/** 为HitResult引用数组排序，排序以按照HitResult与给定位置间的距离排列 */
+	UFUNCTION(BlueprintCallable, Category = "LegendSystem|Algorith")
+	static TArray<FHitResult> HitResultArray_SortByDistance(TArray<FHitResult> InArray, FVector InLocation, bool bOrderFromSmall = true);
 
 #pragma endregion
 	
